8fd1047476e4ac9591d25cafad5abac2b7474e53
Denis Selyaev <denis@selyaev.ru>
2024-03-14 17:23:42 +0300

    Удалить __init__.py

--- utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import get_meme_images
-from . import set_commands
-from . import check_subscribe
-from . import send_zhabka
-from . import schedule_checker
-from . import valid_time_format
-from . import list_admins
