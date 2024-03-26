d4ad7f69ea5688334a41445efc1613705e97124b
Seliaev <selyaevden@yandex.ru>
2024-03-26 23:22:17 +0300

    Исправлен баг, где при каждой проверке, если закончился ключ - пользователю писались сообщения.

--- core/check_time_subscribe.py
+++ core/check_time_subscribe.py
@@ -2,6 +2,8 @@ import asyncio
 from datetime import datetime
 from aiogram import Bot
 
+from core.sql.users_vpn import get_premium_status
+
 
 async def check_time_subscribe(date: datetime) -> bool:
     """
@@ -48,18 +50,19 @@ async def finish_set_date_and_premium() -> None:
 
     :return: None
     """
-    from core.sql.users_vpn import (get_all_records_from_table_users, set_premium_to_table_users,
+    from core.sql.users_vpn import (get_all_records_from_table_users, set_premium_status,
                                     set_date_to_table_users, set_key_to_table_users)
     from core.bot import bot, olm
     all_records = await get_all_records_from_table_users()
     all_finish_records = await get_and_check_records(all_records)
     if all_finish_records:
         for record in all_finish_records:
-            await set_key_to_table_users(account=record.account, value_key=None)
-            await set_premium_to_table_users(account=record.account, value_premium=False)
-            await set_date_to_table_users(account=record.account, value_date=None)
-            olm.delete_key_from_ol(id_user=str(record.account))
-            await send_notification_to_user(bot=bot, id_user=record.account)
+            if await get_premium_status(account=record.account):
+                await set_key_to_table_users(account=record.account, value_key=None)
+                await set_premium_status(account=record.account, value_premium=False)
+                await set_date_to_table_users(account=record.account, value_date=None)
+                olm.delete_key_from_ol(id_user=str(record.account))
+                await send_notification_to_user(bot=bot, id_user=record.account)
 
 
 async def main_check_subscribe() -> None:
